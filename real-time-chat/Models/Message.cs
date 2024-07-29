ac34178801e9a6c535a9acb1603079103f77e5da
Denys <gusardavydov2004@gmail.com>
2024-07-29 13:29:02 +0300

    add sentiment text analysis

--- real-time-chat/Models/Message.cs
+++ real-time-chat/Models/Message.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace real_time_chat.Models;
 
@@ -8,6 +9,7 @@ public class Message
     public int Id { get; set; }
     public string? Text { get; set; }
     public DateTime SentDate { get; set; }
+    public string? Sentiment { get; set; }
     
     public int UserId { get; set; }
     public User User { get; set; }
---
+++ real-time-chat/TextAnalysis/TextAnalysis.cs
@@ -0,0 +1,26 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+
+namespace real_time_chat.TextAnalysis;
+
+public class TextAnalysis
+{
+    private static readonly AzureKeyCredential Credential = new AzureKeyCredential("3a0fe0740e7441139eade13bab2f7905");
+    private static readonly Uri Endpoint = new Uri("https://azureai-textanalysis.cognitiveservices.azure.com/");
+    private readonly TextAnalyticsClient _client;
+
+    public TextAnalysis()
+    {
+        this._client = new TextAnalyticsClient(Endpoint, Credential);
+    }
+
+    public string SentimentAnalysis(string text)
+    {
+        DocumentSentiment documentSentiment = _client.AnalyzeSentiment(text);
+
+        return documentSentiment.Sentiment.ToString();
+
+    }
+    
+    
+}
